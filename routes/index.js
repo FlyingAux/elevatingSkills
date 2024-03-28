8d13f9752c3b27138de7bea33a0ed0395bbe3453
FlyingAux <rajdevpc0420@gmail.com>
2024-03-28 12:17:39 +0530

    mongo worked

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var  userModel = require('./users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// we cannot use one route data to another route 
+// flash messages apko ye allow krte h ki app is route me bane hue data ko dusre route me use kr sako
+// setting up flash message route 
+router.get('/failed',function(req,res,next){
+  req.flash('Done', 5);
+  req.flash('Name', 'raj');
+  res.send('flash created');
+
+})
+
+// checking flash data
+router.get('/checking',function(req,res,next){
+  console.log(req.flash('Done'), req.flash('Name'));
+  res.send('Check your console');
+})
+
+
+// solving mongodb questions
+router.get('/create',async function(req,res,next){
+  const users = await userModel.create({
+    username: 'devraj shrivastava',
+    nickname: 'dev',
+    desc: 'over powered OP',
+    categoreis: ['node.js','GShock', 'React.next.js','JS']
+  })
+  res.send(users)
+})
+
+router.get('/allusers',async function(req,res,next){
+  const users = await userModel.find()
+  res.send(users)
+})
+
+// using regexp to find users using case insensetive capital small letters wont matter
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+var mongoose =  require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+mongoose.connect('mongodb://127.0.0.1:27017/elevateSkills');
+const userSchema = mongoose.Schema({
+  username: String,
+  nickname: String,
+  desc: String,
+  categoreis: {
+    type: Array,
+    default: []
+  },
+  datecreated: {
+    type: Date,
+    default: Date.now()
+  }
+})
 
-module.exports = router;
+module.exports = mongoose.model('user', userSchema)
+
+ 
+
